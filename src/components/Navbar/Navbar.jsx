8543b217f9b3e5c207ce9473b332f44ef0a5b0b5
Sujitthorat1 <136691045+Sujitthorat1@users.noreply.github.com>
2024-02-28 09:33:22 +0530

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-import './navbar.scss'
+import React from 'react';
+import './navbar.scss';
+// import { mdOutlineTravelExplore } from 'react-icons/md';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <section className="navBarSection">
+
+      <header className="header flex">
+        <div className="logoDev">
+          <a href="" className="logo flex">
+            <h1 className="icon">MPCL</h1>
+          </a>
+
+
+        </div>
+
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">Home</a></li>
+            <li className="navItem">
+              <a href="#" className="navLink">About</a></li>
+            <li className="navItem">
+              <a href="#" className="navLink">Career</a></li>
+            <li className="navItem">
+              <a href="#" className="navLink">Investors</a></li>
+            <li className="navItem">
+              <button className="btn">
+                <a href="" >BLOG</a>
+              </button>
+
+            </li>
+          </ul>
+
+          <div className="classNavbar">
+            <AiFillCloseCircle className='icon' />
+          </div>
+        </div>
+      </header>
+    </section>
   )
 }
 
